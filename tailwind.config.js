4306f8dce9dfabc55710985b50af669327d5dd48
sevaho <sebastiaan@sevaho.io>
2024-09-24 20:51:23 +0200

    Add better colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./hugo_stats.json"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+
+        // PRIMARY
+        'primary-green-1': '#51a37a',
+        'primary-green-2': '#408362',
+        // SECONDARY
+        'secondary-green-1': '#b7d0c4',
+        // ACCENT
+        'accent-1': '#89b29e',
+        'accent-2': '#5e905f',
+      }
+    },
   },
   plugins: [require("daisyui")],
   daisyui: {
